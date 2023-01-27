c621300db3083d48687675ea5a20ec24bd18c635
Stella Artois <officedev@gmx.ch>
2023-01-27 11:18:28 +0100

    Added tests

---
+++ Textile.Test/CorpusTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace Textile.Test
+{
+    public class CorpusTest
+    {
+        [TestCaseSource(typeof(CorpusTestData))]
+        public void TestStuff(string input, string expected)
+        {
+            var outputter = new StringBuilderOutputter();
+            var formatter = new TextileFormatter(outputter);
+            formatter.Format(input);
+            var actual = outputter.GetFormattedText();
+
+            Bitmap diff;
+            if (!actual.RendersEqual(expected, out diff))
+            {
+                var path = Path.Combine(Path.GetTempPath(), "Textile.Test");
+
+                if (Directory.Exists(path))
+                {
+                    
+                }
+
+                Directory.CreateDirectory(path);
+                var filename = Path.Combine(path, $"{TestContext.CurrentContext.Test.Name}.png");
+
+                diff.Save(filename);
+
+                Assert.Fail($"HTML does not match.\r\nExpected:\r\n{expected}\r\n\r\nActual:\r\n{actual}\r\n\r\nDiff image: {filename}");
+            }
+        }
+    }
+}
---
+++ UsageTest/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace UsageTest
+{
+
+
+    class Program
+    {
+
+
+        // Textile from dotnetprojects 
+        // https://github.com/dotnetprojects/textile.net
+        // Tests from John Radke 
+        // https://github.com/johnradke/NTextile
+        static void Main(string[] args)
+        {
+            string input = "This _text_ is going to be *formatted* using %{color=#f00;}Textile.NET%.";
+            string result = Textile.TextileFormatter.FormatString(input);
+            System.Console.WriteLine(result);
+
+
+            System.Console.WriteLine(" --- Press any key to continue --- ");
+            System.Console.ReadKey();
+        }
+
+
+    }
+
+
+}
